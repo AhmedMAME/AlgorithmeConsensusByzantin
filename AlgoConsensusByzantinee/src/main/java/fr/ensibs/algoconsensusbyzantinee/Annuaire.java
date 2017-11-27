b4b5baba96c433b75b68f0e17a43995c9f3b6ad2
Figuigui <mehdifiguigui5@gmail.com>
2017-11-27 15:44:49 +0100

    Infra Projet Java

---
+++ AlgoConsensusByzantinee/src/main/java/fr/ensibs/algoconsensusbyzantinee/Annuaire.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.ensibs.algoconsensusbyzantinee;
+
+import java.security.PublicKey;
+import java.util.HashMap;
+
+/**
+ *
+ * @author Mehdi
+ */
+public class Annuaire extends HashMap<PublicKey, Long>{
+    
+}
