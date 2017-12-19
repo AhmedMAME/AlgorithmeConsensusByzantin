d923fba47cfc8b32c21089fe071b3be932e5a434
Tahera56 <guyard.tristan.56@gmail.com>
2017-12-19 21:12:32 +0100

    Ajout Tristan

--- AlgoConsensusByzantinee/src/main/java/fr/ensibs/algoconsensusbyzantinee/Commandant.java
+++ AlgoConsensusByzantinee/src/main/java/fr/ensibs/algoconsensusbyzantinee/Commandant.java
@@ -5,15 +5,19 @@
  */
 package fr.ensibs.algoconsensusbyzantinee;
 
+import java.net.Socket;
+
 /**
  *
  * @author Mehdi
  */
 public class Commandant extends Process{
     public Byte[] intialMsg; 
+    public Socket _socket;
 
-    public Commandant(Boolean isFaulty) {
+    public Commandant(Boolean isFaulty,Socket _socket) {
         super(isFaulty);
+        this._socket = _socket;
     }
     
     public void sendMessage(Byte[] msg) {
