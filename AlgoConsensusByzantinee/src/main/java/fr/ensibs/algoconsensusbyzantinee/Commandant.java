12df732c231d7b4f8909ec2276c4e21fe16a1f06
Figuigui <mehdifiguigui5@gmail.com>
2017-12-31 20:31:36 +0100

    quelques rajout

--- AlgoConsensusByzantinee/src/main/java/fr/ensibs/algoconsensusbyzantinee/Commandant.java
+++ AlgoConsensusByzantinee/src/main/java/fr/ensibs/algoconsensusbyzantinee/Commandant.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package fr.ensibs.algoconsensusbyzantinee;
 
 /**
@@ -12,10 +7,17 @@ package fr.ensibs.algoconsensusbyzantinee;
 public class Commandant extends Process{
     public Byte[] intialMsg; 
 
+    /*
+    
+    */
     public Commandant(Boolean isFaulty) {
         super(isFaulty);
     }
     
+    
+    /*
+    
+    */
     public void sendMessage(Byte[] msg) {
         
     }
--- AlgoConsensusByzantinee/src/main/java/fr/ensibs/algoconsensusbyzantinee/Process.java
+++ AlgoConsensusByzantinee/src/main/java/fr/ensibs/algoconsensusbyzantinee/Process.java
@@ -1,12 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package fr.ensibs.algoconsensusbyzantinee;
 
+import java.security.Key;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
 import java.security.PublicKey;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -21,11 +22,29 @@ public class Process extends Thread {
         this.isFaulty = isFaulty;
     }
     
-    public void generateKey() {
-        
+    /*
+    
+    */
+    public KeyPair generateKey() {
+        try {
+            KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA");
+            kpg.initialize(2048);
+            KeyPair kp = kpg.generateKeyPair();
+            //privateKey = kp.getPrivate();
+            //publicKey = kp.getPublic();
+            //byte[] prk = privateKey.getEncoded();
+            //byte[] pbk = publicKey.getEncoded();
+            return kp;
+        } catch (NoSuchAlgorithmException ex) {
+            Logger.getLogger(Process.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
     }
     
+    /*
+    
+    */
     public void publishKey(Long id, PublicKey publicKey) {
-        
+        publicKey = generateKey().getPublic();
     }
 }
